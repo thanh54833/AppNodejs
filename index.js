33de7736f37893d57dec528f856362d56bb68d86
the gioi cua thanh <pham54833@gmail.com>
2020-09-24 23:46:31 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,85 @@
+
+
+
+
+console.log("hello world ...")
+
+// // var myObject = { name: "thanh", age: "25" }
+// // myObject.name = "Pham Hoai Thanh"
+// // myObject.age = "30"
+// function showContent() {
+//     console.log("content funtion :...")
+// }
+// showContent()
+// var list = ["pham", "hoai", "thanh"]
+// list.push("thanh2")
+// var value = 5
+// console.log(typeof value)
+// //expected output ...
+// //remainder ...
+// //expression ...
+// console.log("list.length :..." + list.length)
+// function log(content) {
+// }
+// console.log("thanh" / 2)
+// //console.log("your name : " + myObject.name + " your age : " + myObject.age)
+// //remainder output : ...
+// function sum(first , last) {
+//     return first + last
+// }
+// console.log("result sum :..  "+sum(1,2)) 
+
+// function callBackFuntion(value) {
+//     console.log("result sum :..." + value)
+// }
+
+// function resultSum(first, last, callBack) {
+//     callBack(first + last)
+//     return first + last
+// }
+
+// resultSum(2, 2, function () {
+//  console.log("call back here:..")
+// })
+
+
+// condition ...
+
+// var value = 11
+// if (value % 2 == 0) {
+//     //Do something...
+//     console.log("result even number ...")
+// } else {
+//     console.log("result not even number ...")
+// }
+
+// switch (10) {
+//     case 1: {
+//         console.log("result value :..." + 1)
+//         break
+//     }
+//     case 2: {
+//         console.log("result value :..." + 2)
+//         break
+//     }
+//     case 2: {
+//         console.log("result value :..." + 3)
+//         break
+//     }
+
+//     default: {
+//         console.log("result value :..." + "default ")
+//     }
+// }
+// Example : ...
+
+var list = ["mot", "hai", "ba"]
+
+
+
+
+
+
+
+
+
